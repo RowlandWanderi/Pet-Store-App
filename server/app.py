d77a916f0a852deea1bc0f6eae55cf4d67d87287
Samuel Kimote <readssamuel@.com>
2024-01-26 09:45:08 +0300

    progress update

--- server/app.py
+++ server/app.py
@@ -7,7 +7,6 @@ from flask_jwt_extended import JWTManager
 import random
 import string
 from views import *
-from gevent.pywsgi import WSGIServer
 
 def generate_secret_key(length=32):
     characters = string.ascii_letters + string.digits
@@ -40,8 +39,6 @@ def token_in_blocklist_callback(jwt_header, jwt_data):
         return None
     
 if __name__ == '__main__':
-    # Debug/Development
-    # app.run(debug=True, host="0.0.0.0", port="5000")
-    # Production
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    # app.run(port=5000, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
